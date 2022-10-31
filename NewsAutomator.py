eabe0e8eb60c15cf567933e34ff110ea24033366
jakub <kkrusicki@gmail.com>
2022-10-31 15:15:50 +0100

    dealt with some confusion of files

--- NewsAutomator.py
+++ NewsAutomator.py
@@ -1,8 +1,10 @@
 import requests
+from sys import argv
 
 API_KEY = 'e6dd1a095f3e496fa0de39c85d2017e0'
 
 URL = 'https://newsapi.org/v2/top-headlines?'
+
 
 def get_articles_by_category(category):
     query_parameters = {
@@ -13,15 +15,28 @@ def get_articles_by_category(category):
     }
     return _get_articles(query_parameters)
 
-#ef get_articles_by_query(query):
+# def get_articles_by_query(query):
 
 
 def _get_articles(parameters):
     response = requests.get(URL, params=parameters)
 
-    articles = response.json("articles")
+    articles = response.json()["articles"]
 
     results = []
 
     for article in articles:
-        results.append()
+        results.append({"title": article["title"], "url": article["url"]})
+
+    for result in results:
+        print(result["title"])
+        print(result["url"])
+        print('')
+
+# def get_sources_by_category(category):
+
+
+if __name__ == "__main__":
+    print(f"Getting news for {argv[1]}...\n")
+    get_articles_by_category(argv[1])
+    print(f"Succesfully retrieved top {argv[1]} headlines")
